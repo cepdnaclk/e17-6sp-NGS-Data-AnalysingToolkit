9133c32795793a0f6417472333e244014eedcc12
Nishankar <66866762+Nishan-Charlie@users.noreply.github.com>
2022-03-04 18:53:36 +0530

    Better table visualize

--- env/NGS3.0/NGSToolKit/register/urls.py
+++ env/NGS3.0/NGSToolKit/register/urls.py
@@ -1,8 +1,11 @@
-from .views import register
+from re import template
+from .views import register, login, profile
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', register, name='Signup'),
-    path('signup', register, name='Signup'),
+    path('login',auth_views.LoginView.as_view(template_name = "login.html"), name="Login"),
+    path("profile", profile, name="Profile" )
     
 ]
--- env/NGS3.0/NGSToolKit/register/views.py
+++ env/NGS3.0/NGSToolKit/register/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def register(request):
-    form = CreateUserForm()
-    return render(request, "register.html",{"form":form})
+    if request.method =="POST":
+        name = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+        
+        if password1 == password2:
+            user = User.objects.create_user(username=name, email=email, password= password1)
+            user.save()
+            messages.success(request, 'Your account has been successfully created')
+            return redirect('Login')
+        else:
+            messages.warning(request,"Password is not match......!")
+            return redirect('Signup')
+    else:
+        form = CreateUserForm()
+        return render(request, "register.html",{"form":form})
 
 
 def login(request):
     return render(request, "register.html")
-    
+
+def profile(request):
+    return render(request, "profile.html",)
